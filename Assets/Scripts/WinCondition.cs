405080d86eb8b4c720953c9b5b5f2920c8a78da7
ra1l0 <agelii.tim@gmail.com>
2021-09-14 15:54:25 +0200

    func
    Collectibles + exit + player movement and collission

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    private int amountToWin;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && other.gameObject.GetComponent<PlayerController>().getCollectedGameObject() == amountToWin)
+        {
+            Debug.Log("You Win");
+
+        }
+
+    }
+    public void addAmountToWin()
+    {
+
+        amountToWin++;
+    }
+
+}

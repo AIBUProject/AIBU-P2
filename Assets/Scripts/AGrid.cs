633cb44e8107a541a46442a4ba9ec5dc42f147ed
ra1l0 <agelii.tim@gmail.com>
2021-09-17 13:50:50 +0200

    Extra väntetid tills griden skapas

--- Assets/Scripts/AGrid.cs
+++ Assets/Scripts/AGrid.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class AGrid : MonoBehaviour
 {
-
+    private float waitTimer = 0;
     public GameObject pathfinder;
     bool doOnce = false;
 
     void Update()
     {
       if(GameObject.Find("RoomController").GetComponentInChildren<RoomController>().spawnedEndRoom == true && doOnce == false){
-            Instantiate(pathfinder, new Vector2(0, 0), Quaternion.identity);
-            doOnce = true;
-
+            if (gridWait()) {
+                Instantiate(pathfinder, new Vector2(0, 0), Quaternion.identity);
+                doOnce = true;
+            }
+          
         }
     }
+
+    private bool gridWait()
+    {
+        //Debug.Log(seconds);
+        waitTimer += Time.deltaTime;
+
+        if (waitTimer >= 2f)
+        {
+
+            waitTimer = 0;
+            return true;
+
+        }
+
+        return false;
+    }
+
 }

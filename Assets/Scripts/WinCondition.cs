1b92eed4fea0c5e079761e7c2ca14d2efbcce513
ra1l0 <agelii.tim@gmail.com>
2021-09-17 14:00:08 +0200

    you can "Win"

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class WinCondition : MonoBehaviour
 {
+    Scene scene;
     GameObject[] collectibles;
     void Start()
     {
+        scene = SceneManager.GetActiveScene();
         collectibles = GameObject.FindGameObjectsWithTag("Collectible");
         amountToWin = collectibles.Length;
     }
     private int amountToWin;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log(collectibles.Length + "to win");
         if (other.tag == "Player" && other.gameObject.GetComponent<PlayerController>().getCollectedGameObject() == amountToWin)
         {
             Debug.Log("You Win");
-
+            SceneManager.LoadScene(scene.name);
         }
 
     }

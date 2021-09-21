3fef148dfea50b258153e3ab14884bcdb04beb93
SchpuunkSWE <75782018+SchpuunkSWE@users.noreply.github.com>
2021-09-21 16:21:16 +0200

    Fixed exit animation!

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -6,11 +6,27 @@ public class WinCondition : MonoBehaviour
 {
     Scene scene;
     GameObject[] collectibles;
+    private GameObject player;
+    public Animator animator;
+    private bool playedAnim = false;
     void Start()
     {
         scene = SceneManager.GetActiveScene();
         collectibles = GameObject.FindGameObjectsWithTag("Collectible");
+        player = GameObject.FindGameObjectWithTag("Player");
         amountToWin = collectibles.Length;
+    }
+
+    void Update()
+    {
+        if(!playedAnim)
+        {
+            if(player.gameObject.GetComponent<PlayerController>().getCollectedGameObject() == amountToWin)
+            {
+                animator.SetTrigger("Open");
+                playedAnim = true;
+            }
+        }
     }
     private int amountToWin;
     private void OnTriggerEnter2D(Collider2D other)

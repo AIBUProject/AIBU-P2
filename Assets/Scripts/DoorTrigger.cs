36b9dc2e8f00a868970d5a3ed6d750645f6832e5
ra1l0 <agelii.tim@gmail.com>
2021-09-22 15:45:14 +0200

    Fungerande troll despawn

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -7,19 +7,28 @@ public class DoorTrigger : MonoBehaviour
 
 
     GameObject[] spawners;
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            
+            if (GameObject.Find("TrollHuntTimer").GetComponent<EnemyHunt>().getHuntTimeDone() == true)
+            {
+
+                GameObject.Find("TrollHuntTimer").GetComponent<EnemyHunt>().setIsHunting(false);
+                GameObject.Find("TrollHuntTimer").GetComponent<EnemyHunt>().setOnCooldown(true);
+                GameObject.Find("TrollHuntTimer").GetComponent<EnemyHunt>().setHuntTimeDone(false);
+
+            }
+
             spawners = GameObject.FindGameObjectsWithTag("Spawner");
             for (int i = 0; i < spawners.Length; i++)
             {
                 Debug.Log("spawner number" + i);
                 spawners[i].GetComponent<EnemySpawner>().setAllowSpawn(true);
             }
-        
+
         }
     }
 
